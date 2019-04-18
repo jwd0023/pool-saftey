d9e44ef51e44078b453753f7d33cbdd196918010
raspi <32208193+jwd0023@users.noreply.github.com>
2019-04-18 06:41:56 +0100

    Changed logging for local testing

--- juicy/test/log_data.py
+++ juicy/test/log_data.py
@@ -1,83 +1,29 @@
 import sys
 import time
-import json
-import datetime
-import urllib.request
-import urllib.parse
+#import json
 from mpu9250.mpu9250 import mpu9250
-
-# API write key.
-THINGSPEAK_API_KEY = "797UKWN6FJXVHF6O"
-
-# Free ThingSpeak limits to 1 API request every 15 seconds. For bulk update (i.e. multiple data points per
-# API request), only 960 data points may be included.
-SEND_DELAY    = 15
-MEASURE_DELAY = SEND_DELAY / (960)
-REQUEST_URL = "https://api.thingspeak.com/channels/753579/bulk_update.json"
-
-
-def bulk_update_channel(write_data):
-    # Format the input data into JSON format.
-    data = {'write_api_key': THINGSPEAK_API_KEY,
-            'updates'      : write_data}
-
-    data = json.dumps(data).encode('utf-8')
-
-    # Form the http request using urllib2.
-    req = urllib.request.Request(url=REQUEST_URL)
-    request_headers = {"User-Agent": "mw.doc.bulk-update (Raspberry Pi)",
-                       "Content-Type": "application/json",
-                       "Content-Length": str(len(data))}
-
-    # Set http request headers.
-    for key, val in request_headers.items():
-        req.add_header(key, val)
-
-    # Actually make the request to ThingSpeak.
-    try:
-        urllib.request.urlopen(req, data=data)
-    except Exception as e:
-        pass
 
 
 if __name__ == "__main__":
 
-    # Initialize sensor with external library class.
+     # Initialize sensor with external library class.
     sensor = mpu9250()
+    log_file = open(str(int(round(time.time()*1000))) + '_juicy.log', 'w')
+    log_file.write('time,ax,ay,az,gx,gy,gy\n')
 
-    # Keep track of measurement and API request timing.
-    last_send_time          = time.time()
-    last_measurement_time   = time.time()
-
-    # Initialize measurement buffer.
-    measurement_buffer = []
-
-    # Keep track of date - needed to log online to ThingSpeak thing.
-    fake_date = datetime.datetime.now()
-    
     # Main loop.
     while True:
 
-        # Only make measurement if it is time to. Limited by ThingSpeak unpaid API.
-        if (time.time() - last_measurement_time) > MEASURE_DELAY:
+        movement = sensor.accel
+        orientation = sensor.gyro
 
-            # Current accelerometer reading.
-            reading = sensor.accel
+        line = str(int(round(time.time()*1000)))
+        line = line + ',{accel[0]},{accel[1]},{accel[2]}'.format(accel=movement)
+        line = line + ',{gyro[0]},{gyro[1]},{gyro[2]}'.format(gyro=orientation)
+        line = line + '\n'
 
-            # Store data to measurement buffer.
-            measurement_buffer.append({'created_at': fake_date.isoformat(),
-                                       'field1': reading[0],
-                                       'field2': reading[1],
-                                       'field3': reading[2]})
-            
-            # Update fake date by one second
-            fake_date = fake_date + datetime.timedelta(seconds=1)
-
-            # Update last measurement time to now.
-            last_measurement_time = time.time()
-
-
-
+        log_file.write(line)
+        time.sleep(0.01)
         # Make url request every WAIT_TIME seconds.
         if (time.time() - last_send_time) > SEND_DELAY:
             # Send bulk update, clear measurement buffer, and update timing information.
